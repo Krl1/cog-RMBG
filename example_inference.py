7b1db373a6816ef22cfa8468ec33eec6a173c663
Karol <karol.kulawiec@spectre.solutions>
2024-12-17 11:35:37 +0100

    refactor and add images

--- example_inference.py
+++ example_inference.py
@@ -1,40 +1,56 @@
+import os
 from skimage import io
 import torch, os
+from pathlib import Path
 from PIL import Image
 from briarmbg import BriaRMBG
 from utilities import preprocess_image, postprocess_image
 from huggingface_hub import hf_hub_download
 
-def example_inference():
 
-    model_path = hf_hub_download("briaai/RMBG-1.4", 'model.pth')
-    im_path = f"{os.path.dirname(os.path.abspath(__file__))}/example_input.jpg"
-
-    net = BriaRMBG()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    net.load_state_dict(torch.load(model_path, map_location=device))
-    net.to(device)
-    net.eval()    
-
+def example_inference(net, device, im_path, out_path):
     # prepare input
-    model_input_size = [1024,1024]
+    model_input_size = [1024, 1024]
     orig_im = io.imread(im_path)
     orig_im_size = orig_im.shape[0:2]
     image = preprocess_image(orig_im, model_input_size).to(device)
 
-    # inference 
-    result=net(image)
+    # inference
+    result = net(image)
 
     # post process
     result_image = postprocess_image(result[0][0], orig_im_size)
 
     # save result
     pil_im = Image.fromarray(result_image)
-    no_bg_image = Image.new("RGBA", pil_im.size, (0,0,0,0))
+    no_bg_image = Image.new("RGBA", pil_im.size, (0, 0, 0, 0))
     orig_image = Image.open(im_path)
     no_bg_image.paste(orig_image, mask=pil_im)
-    no_bg_image.save("example_image_no_bg.png")
+    no_bg_image.save(out_path)
+
+
+def main():
+    net = BriaRMBG()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model_path = hf_hub_download("briaai/RMBG-1.4", "model.pth")
+    net.load_state_dict(torch.load(model_path, map_location=device))
+    net.to(device)
+    net.eval()
+
+    image_directory = "../../images/"
+    outs_path = "../../outputs/cog-RMBG/"
+    for filename in os.listdir(image_directory):
+        if filename.lower().endswith(
+            (".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tiff")
+        ):
+
+            im_path = image_directory + filename
+            out_path = outs_path + filename
+            out_path = Path(out_path)
+            out_path = out_path.with_suffix(".png")
+
+            example_inference(net, device, im_path, out_path)
 
 
 if __name__ == "__main__":
-    example_inference()
+    main()
